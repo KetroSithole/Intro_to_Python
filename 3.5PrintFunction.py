aabcb8b70f73d10e40d8a5446ff4d31c58abcb6b
Thichaona Sithole <thichaona.sithole@sigmaconnected.com>
2025-11-06 21:45:46 +0200

    testing the print fucntiopn

---
+++ 3.5PrintFunction.py
@@ -0,0 +1,11 @@
+# print("This is me testing how to use the print fucntion")
+# print("1+1")  #not going to print 2
+# print(1+1)
+# print('1+1')
+
+# a='Hi my name is '
+# b='Ketro Sithole '
+a=7
+b=2
+c=a+b
+print(a//b)
