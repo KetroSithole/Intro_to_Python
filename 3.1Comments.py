74dc88f4b6219b417c73558d87d60f0b12d1e30a
Thichaona Sithole <thichaona.sithole@sigmaconnected.com>
2025-11-06 21:34:26 +0200

    BASIC CALC

---
+++ 3.1Comments.py
@@ -0,0 +1,6 @@
+# This is a comment (Allows u to define what a project does or ignore a code block )
+
+a=1
+b=2
+Total=a+b
+print(Total)
---
+++ 3.2Basic_Calc.py
@@ -0,0 +1,22 @@
+# print(10+2) #12
+# print(10-2) #8
+# print(10*2) #20
+# print(10/2) #5.0
+
+########Function        (DRY OR KISS )
+# def add(a,b):
+#     print(a+b)
+
+# add(5,5)
+# add(5,8)
+# add(4,5)
+
+#Modulu  2 power 3
+#print(2**3)
+
+def function1(a,b):
+    print(a**b)
+
+function1(2,3)    #2x2x2=8
+
+
