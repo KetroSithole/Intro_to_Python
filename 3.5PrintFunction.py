1e7cda030cc9b45a4f5c3b4c0ae5b6eab42a676d
Thichaona Sithole <thichaona.sithole@sigmaconnected.com>
2025-11-07 12:40:29 +0200

    Refactor print function demonstration and restore commented code

--- 3.5PrintFunction.py
+++ 3.5PrintFunction.py
@@ -1,13 +1,27 @@
-# print("This is me testing how to use the print fucntion")
-# print("1+1")  #not going to print 2
-# print(1+1)
-# print('1+1')
+# Demonstrating how to use the print() function in Python
 
-# a='Hi my name is '
-# b='Ketro Sithole '
-a=7
-b=2
-c=a+b
-print(a//b)
+# Printing text
+print("This is me testing how to use the print function")
 
-#########################################Messinmg rthe code 
+# Printing text vs arithmetic
+print("1+1")   # Will print the text '1+1'
+print(1+1)     # Will print the result of the addition (2)
+print('1+1')   # Will also print the text '1+1'
+
+# Working with variables
+a = 'Hi my name is '
+b = 'Ketro Sithole '
+print(a + b)   # Concatenates (joins) the two strings and prints them
+
+# Performing arithmetic operations
+a = 7
+b = 2
+c = a + b
+print("The sum of a and b is:", c)
+
+# Integer (floor) division
+print("Integer division (a // b):", a // b)
+
+# End of program
+#########################################
+# Messing with the code :)
